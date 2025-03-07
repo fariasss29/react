321bdee007c92b0754b0536f38174c454b99eb4f
fariasss29 <162914687+fariasss29@users.noreply.github.com>
2025-03-07 14:29:48 -0300

    Create app.jsx

---
+++ app.jsx
@@ -0,0 +1,16 @@
+import Titulo from "./Titulo"
+
+function App() {
+  return(
+  <div>
+  <Titulo/>
+  <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum perferendis quaerat optio praesentium, eius modi dolor sapiente, qui, illum voluptas neque ut quam? Dolorum est hic sit perspiciatis animi commodi.</p>
+  <Titulo/>
+  <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum perferendis quaerat optio praesentium, eius modi dolor sapiente, qui, illum voluptas neque ut quam? Dolorum est hic sit perspiciatis animi commodi.</p>
+  <Titulo/>
+  <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum perferendis quaerat optio praesentium, eius modi dolor sapiente, qui, illum voluptas neque ut quam? Dolorum est hic sit perspiciatis animi commodi.</p>
+  </div>
+  )
+}
+
+export default App;
